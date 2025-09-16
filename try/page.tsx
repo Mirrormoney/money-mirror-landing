291ca488cff893b2b1721b4dfe984e985560a279
Moneymirror <mai-sven@gmx.net>
2025-09-16 16:45:03 +0200

    chore: remove legacy /try folder

--- try/page.tsx
+++
@@ -1,5 +0,0 @@
-// Legacy placeholder to avoid TS build errors if this folder exists.
-// This file is NOT used by Next.js App Router. You can safely delete the entire /try folder.
-export default function TryLegacyPlaceholder() {
-  return null;
-}
